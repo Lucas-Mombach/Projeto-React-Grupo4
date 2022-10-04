d8eaf44822e01934ec6faae1090a418ca758c607
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2022-10-04 12:46:21 -0300

    Tabela funcionando

--- ReactProjects/my-app/src/App.jsx
+++ ReactProjects/my-app/src/App.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import NovoTipo from './components/NovoTipo'
 import Cadastro from './components/Cadastro'
+import Tabela from './components/Tabela'
 
 export default _ =>
 
     <div className="App">
         <h1>Padaria Victoria</h1>
 
-        <NovoTipo></NovoTipo>
-        <Cadastro></Cadastro>
-        
+        <NovoTipo />
+        <Cadastro />
+        <Tabela />
 
     </div>
---
+++ ReactProjects/my-app/src/components/Tabela.jsx
@@ -0,0 +1,64 @@
+import './Tabela.css';
+import React from 'react';
+import listaProdutos from '../data/mercadorias'
+
+export default props => {
+    return (
+        <div className="TabelaProdutos">
+            <hr />
+            <button onClick={
+                function() {
+                    let tbody = document.getElementById("tbody");
+
+                    for (let i = 0; i < listaProdutos.length; i++) {
+                        let tr = tbody.insertRow();
+                        let td_id = tr.insertCell();
+                        let td_nome = tr.insertCell();
+                        let td_tipo = tr.insertCell();
+                        let td_dataDeFabricacao = tr.insertCell();
+                        let td_dataDeValidade = tr.insertCell();
+                        let td_peso = tr.insertCell();
+                        let td_custoDeFabricacao = tr.insertCell();
+                        let td_precoDeVenda = tr.insertCell();
+                        let td_qtdeProduzida = tr.insertCell();
+                        let td_qtdeVendida = tr.insertCell();
+                        let td_funcionario = tr.insertCell();
+                        let td_lucro = tr.insertCell();
+
+                        td_id.innerText = i;
+                        td_nome.innerText = listaProdutos[i].nome;
+                        td_tipo.innerText = listaProdutos[i].tipo;
+                        td_dataDeFabricacao.innerText = listaProdutos[i].dataDeFabricacao;
+                        td_dataDeValidade.innerText = listaProdutos[i].dataDeValidade;
+                        td_peso.innerText = listaProdutos[i].peso;
+                        td_custoDeFabricacao.innerText = listaProdutos[i].custoDeFabricacao;
+                        td_precoDeVenda.innerText = listaProdutos[i].precoDeVenda;
+                        td_qtdeProduzida.innerText = listaProdutos[i].qtdeProduzida;
+                        td_qtdeVendida.innerText = listaProdutos[i].qtdeVendida;
+                        td_funcionario.innerText = listaProdutos[i].funcionario;
+                        td_lucro.innerText = listaProdutos[i].lucro;
+                    }
+                }
+                }>Atualizar Tabela</button>
+            
+            <table border="1">
+                <thead>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Tipo</th>
+                    <th>Data de Fabricação</th>
+                    <th>Data de Validade</th>
+                    <th>Peso</th>
+                    <th>Custo de Fabricação</th>
+                    <th>Preço de Venda</th>
+                    <th>Qtde Prduzida/Dia</th>
+                    <th>Qtde Vendida/Dia</th>
+                    <th>Funcionário</th>
+                    <th>Lucro</th>
+                </thead>
+                <tbody id="tbody" />
+            </table>
+            
+        </div>
+    );
+}
