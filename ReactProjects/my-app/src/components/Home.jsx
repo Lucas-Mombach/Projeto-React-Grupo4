9e5ff9452b670aac4cebf9254b871ba6d0035528
Lucas-Mombach <lucasmmombach@gmail.com>
2022-10-05 10:17:27 -0300

    Home and Menu updated

---
+++ ReactProjects/my-app/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import './Cadastro.css'
+import React from 'react';
+import listaProdutos from '../data/mercadorias'
+import listaTipos from '../data/tipos'
+
+export default props => {
+    
+    return (
+        <div>
+            <main>
+              <h1> Welcome </h1>
+            </main>
+        </div>
+    );
+}
---
+++ ReactProjects/my-app/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import './Cadastro.css'
+import React from 'react';
+
+export default props => {
+    
+    return (
+        <aside>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/'>Login</a></li>
+            <li><a href='/'>Cadastros</a></li>
+            <li><a href='/'>Produtos</a></li>
+          </ul>
+        </aside>
+    );
+}
