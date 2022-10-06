e549a4968e6f15d064a00f50d074efcec80fd75a
Lucas-Mombach <lucasmmombach@gmail.com>
2022-10-05 22:33:24 -0300

    Home is now whith proper style

--- ReactProjects/my-app/src/components/Home.jsx
+++ ReactProjects/my-app/src/components/Home.jsx
@@ -1,20 +1,17 @@
-import './Cadastro.css'
+import './Home.css'
 import React from 'react';
-import listaProdutos from '../data/mercadorias'
-import listaTipos from '../data/tipos'
-
 export default props => {
     
     return (
         <div className="Home">
-        <header>
-          <h1>Padaria Victoria</h1>
-          <p>
-            Sistema de Gerenciamento de estoque de produtos da Padaria Victoria
-          </p>
-         <button type='button'>Login</button>
-         
-        </header>
-      </div>
+            <header>
+            <h1>Padaria Victoria</h1>
+            <p>
+                Sistema de Gerenciamento de estoque de produtos da Padaria Victoria
+            </p>
+            <button type='button'><a href='/login'>Login</a></button>
+            
+            </header>
+        </div>
     );
 }
